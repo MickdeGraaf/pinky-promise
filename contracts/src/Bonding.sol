e8fe10dc8ca824f17b65d3fc9cdfff1652e0d538
pedrovalido <pedro.validex@gmail.com>
2023-11-18 13:16:37 +0300

    style: format

--- contracts/src/Bonding.sol
+++ contracts/src/Bonding.sol
@@ -5,7 +5,6 @@ import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Bonding {
-
     // struct Verifier {
     //     address addressOnDestChain;
     //     address tokenOnDestChain; // address(0) for Native on dest chain
