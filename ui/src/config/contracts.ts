953e2ff8707f6f214f322027bec97872a246ca3e
Mick de Graaf <mick@mickdegraaf.nl>
2023-11-18 21:35:11 +0100

    Update ABI and address of Bonding

--- ui/src/config/contracts.ts
+++ ui/src/config/contracts.ts
@@ -1,5 +1,5 @@
 export default {
-    bonding: "0x2fedc1E12F137982d3D4F3e07Ef8a2a480F7b35E",
+    bonding: "0x8aF40Fbc05f084522df22042006BB26d7b42e373",
     bondingChainId: 5,
     fulfillers: {
         // TODO actual deployments
